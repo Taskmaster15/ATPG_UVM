3079f067989d85f833307f6553d204332daf4b22
Taskmaster15 <113618751+Taskmaster15@users.noreply.github.com>
2026-05-13 11:12:49 +0530

    Update atpg_cov.sv

--- atpg_cov.sv
+++ atpg_cov.sv
@@ -10,9 +10,9 @@ class atpg_cov extends uvm_subscriber #(atpg_item);
     cp_fault_en:   coverpoint seq.fault_en  { bins off = {0}; bins on  = {1}; }
     cp_fault_type: coverpoint seq.fault_type{ bins sa0 = {0}; bins sa1 = {1}; }
     
-    cp_algo: 	   coverpoint seq.algo_sel  { bins prng = {atpg_item::PRNG};
-  											  bins fdg  = {atpg_item::FDG};
-  											  bins wpg  = {atpg_item::WPG};}
+    cp_algo: 	   coverpoint seq.algo_sel  { bins prng = {PRNG};
+  					      bins fdg  = {FDG};
+  				              bins wpg  = {WPG};}
     
     cp_ext:        coverpoint seq.ext       { bins no_carry   = {4'b0000};
                                               bins full_carry = {4'b1111};
@@ -20,14 +20,19 @@ class atpg_cov extends uvm_subscriber #(atpg_item);
 
   
     cross_algo_fault: cross cp_algo, cp_fault_en, cp_fault_type {
-      ignore_bins fdg_no_fault = binsof(cp_algo.fdg)     && binsof(cp_fault_en.off);
-      ignore_bins no_fault_sa1 = binsof(cp_fault_en.off) && binsof(cp_fault_type.sa1);
-    }
-    cross_fault_output: cross cp_fault_en, cp_fault_type, cp_out {
-      ignore_bins no_fault_sa1 = binsof(cp_fault_en.off) && binsof(cp_fault_type.sa1);
-    }
-    cross_algo_output: cross cp_algo, cp_out;
-    cross_algo_carry:  cross cp_algo, cp_ext;
+  ignore_bins fdg_no_fault = binsof(cp_algo.fdg)     && binsof(cp_fault_en.off);
+  ignore_bins no_fault_sa1 = binsof(cp_fault_en.off) && binsof(cp_fault_type.sa1);
+}
+cross_fault_output: cross cp_fault_en, cp_fault_type, cp_out {
+  ignore_bins no_fault_sa1 = binsof(cp_fault_en.off) && binsof(cp_fault_type.sa1);
+}
+cross_algo_output: cross cp_algo, cp_out {
+  ignore_bins fdg_unreachable = binsof(cp_algo.fdg) &&
+                                binsof(cp_out.out_bins) intersect {[2:13]};
+}
+cross_algo_carry: cross cp_algo, cp_ext {
+  ignore_bins fdg_no_partial = binsof(cp_algo.fdg) && binsof(cp_ext.partial);
+}
   endgroup
 
   function new(string name = "atpg_cov", uvm_component parent = null);
